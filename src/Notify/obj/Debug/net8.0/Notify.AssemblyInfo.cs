98208922b9edc4e1568785f30b0e3072c1d29212
alirezadoostdar <alireza.doostdar@gmail.com>
2024-07-19 18:25:02 +0330

    add email trace class

--- src/Notify/obj/Debug/net8.0/Notify.AssemblyInfo.cs
+++ src/Notify/obj/Debug/net8.0/Notify.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Notify")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35003c2c3935b90921c4f24abcbaa8ef05d49e09")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4dc3ad9f4313ae56154462ce5fd7ccbee10a6913")]
 [assembly: System.Reflection.AssemblyProductAttribute("Notify")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Notify")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
